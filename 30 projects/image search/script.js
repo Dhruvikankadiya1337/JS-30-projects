5f346a3a9aad4fcd2200c2de05a34a12289634f5
Dhruvikankadiya1337 <dhruvikankadiya@gmail.com>
2025-04-29 17:15:19 +0530

    Add files via upload

---
+++ 30 projects/image search/script.js
@@ -0,0 +1,5 @@
+const searchbar = document.getElementById("searchbar");
+const searchbox = document.getElementById("searchbox");
+const result = document.getElementById("result");
+const morebtn = document.getElementById("morebtn");
+
